27e0a3789601836b28477314e84ff547e8e5b58c
Matthew Hickson <matthewhickson@outlook.com>
2022-04-29 21:34:44 -0400

    sane indentation added (I hope)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,11 +7,22 @@
 
 (delete-selection-mode 1)
 
-(electric-indent-mode 0)
-(set-default 'tab-always-indent 'complete)
-(setq indent-tabs-mode t)
-(setq tab-width 4)
-(setq sentence-end-double-space nil)
+(setq custom-tab-width 4)
+
+(defun disable-tabs () (setq indent-tabs-mode nil))
+(defun enable-tabs ()
+  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
+  (setq indent-tabs-mode t)
+  (setq 'tab-always-indent 'complete)
+  (setq tab-width custom-tab-width))
+
+(add-hook 'prog-mode-hook 'enable-tabs)
+(add-hook 'lisp-mode-hook 'disable-tabs)
+(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+
+(setq-default electric-indent-inhibit t)
+
+(setq backward-delete-char-untabify-method 'hungry)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
